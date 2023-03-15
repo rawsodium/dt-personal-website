ec7b88dee0018b3b854d16072966d1411b51b07c
rawsodium <deannat2003@gmail.com>
2023-03-15 14:34:17 -0400

    fixed entrypoint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
   
 const Home = () => {
   return (
     <div>
-        <h1>welcome :)</h1>
+        <Fade down>
+            <h1>welcome :)</h1>
+        </Fade>
     </div>
   );
 };
