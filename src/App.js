9b5cac8dddef9672a8ce191ecb617abfcbe93c4d
rawsodium <deannat2003@gmail.com>
2023-03-17 17:22:41 -0400

    i broke it again

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Routes, Route, BrowserRouter}
-    from 'react-router-dom';
-//import Home from './pages';
+import { BrowserRouter as Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages';
 import About from './pages/about';
 import Projects from './pages/projects';
 import Experience from './pages/experience';
@@ -15,7 +14,8 @@ return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
-            <Route path='/' element={<About/>} />
+            <Route path='/' exact element={<Home />} />
+            <Route path='/about' element={<About/>} />
             <Route path='/projects' element={<Projects/>} />
             <Route path='/experience' element={<Experience/>} />
             <Route path='/creations' element={<Creations/>} />
