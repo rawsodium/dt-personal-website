9a767a18a4c38eb9852ffc9c09995d26a2c69832
rawsodium <deannat2003@gmail.com>
2023-03-25 15:54:25 -0400

    fixed paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/App.css';
+import './styles/App.css';
 import Home from './pages/Home';
 import Navbar from './pages/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
