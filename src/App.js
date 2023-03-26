3230c6fa93706272f10bb3ed589f47c5e30a062f
rawsodium <deannat2003@gmail.com>
2023-03-26 01:46:24 -0400

    fixed file cases

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles/App.css';
 import Home from './pages/Home';
-import Navbar from './pages/Navbar';
+import Navbar from './pages/navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Projects from './pages/projects';
-import Experience from './pages/Experience';
-import Creations from './pages/Creations';
-import NotFound from './pages/NotFound'
-import Footer from './pages/Footer';
+import Experience from './pages/experience';
+import Creations from './pages/creations';
+import NotFound from './pages/notFound'
+import Footer from './pages/footer';
 
 function App() {
   return (
