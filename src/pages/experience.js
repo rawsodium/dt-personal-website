a751c36d78c05fd69b0af45af13d1950f27b2e45
Deanna Turner <99517267+rawsodium@users.noreply.github.com>
2025-10-24 01:05:04 -0400

    Update experience.js 10-2025

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -7,6 +7,12 @@ const Classes =
     <h1><Fade cascade duration="250" triggerOnce>Relevant Coursework</Fade></h1>
         <Fade direction="up" triggerOnce>
         <p>An exhaustive list of courses I've taken during my time at Northeastern.</p>
+        <b>Graduate:</b>
+        <ul>
+            <li>CS 5520 - Mobile Application Development</li>
+            <li>CS 6620 - Fundamentals of Cloud Computing</li>
+        </ul>
+        <b>Undergraduate:</b>
         <ul>
             <li>CS 1800 - Discrete Structures</li>
             <li>CS 2500 - Fundamentals of Computer Science 1</li>
@@ -14,12 +20,16 @@ const Classes =
             <li>CS 3500 - Object-Oriented Design</li>
             <li>CS 3650 - Computer Systems</li>
             <li>CS 5200 - Database Management Systems (Graduate Level)</li>
+            <li>CS 5340 - Computer/Human Interaction (Graduate Level)</li>
+            <li>CS 5500 - Foundations of Software Engineering (Graduate Level)</li>
             <li>CS 5800 - Algorithms (Graduate Level)</li>
             <li>CY 2550 - Foundations of Cybersecurity</li>
             <li>MUST 1220 - Introduction to Music Technology</li>
             <li>MUST 2102 - Composing with Digital Technologies</li>
             <li>MUST 2431 - Computer Music Fundamentals</li>
             <li>MUST 3603 - Embedded Audio Programming</li>
+            <li>MUST 4610 - Composition for Electronic Instruments</li>
+            <li>MUST 4611 - Music Technology Capstone</li>
             <li>MUSC 1001 - Music in Everyday Life</li>
             <li>MUSC 1119 - Fundamentals of Western Music Theory</li>
             <li>MUSC 1201 - Music Theory 1</li>
@@ -28,6 +38,11 @@ const Classes =
             <li>MUSC 2350 - Acoustics and Psychoacoustics of Music</li>
             <li>MUSC 3300 - Music Perception and Cognition</li>
             <li>MUSI 2232 - Music Recording 1</li>
+            <li>MUSI 2331 - Music Production and Recording 2</li>
+            <li>MUSI 3341 - Music Production and Recording 3 - Mixing and Mastering</li>
+            <li>ENGW 3302 - Advanced Writing in the Technical Professions</li>
+            <li>PSYC 1101 - Foundations of Psychology</li>
+            <li>PHIL 1145 - Technology and Human Values</li>
         </ul>
     </Fade>
 </div>
@@ -37,9 +52,9 @@ const Technical =
     <h1><Fade cascade duration="250" triggerOnce>Technical Knowledge</Fade></h1>
         <Fade direction="down" triggerOnce>
             <h2>Languages</h2>
-                <p>Java, Python, JavaScript, HTML/CSS, C++, C, Bash, SQL, TypeScript, Racket/Lisp </p>
+                <p>JavaScript (Vue/React/Node), Python, C++, Java, Bash, C, SQL (MySQL), TypeScript, Swift, Racket/Lisp </p>
             <h2>Frameworks/Tools</h2>
-                <p>Vue, Node, React, Git, Docker, Elasticsearch, Vim, JUnit, MaxMSP, Logic Pro, Pro Tools</p>
+                <p>Git, Docker, Elasticsearch, MongoDB, JUCE, OpenShift, Kubernetes, AWS, LangChain, MaxMSP, Vim, JUnit, Logic Pro, Pro Tools, Jest</p>
             <h2>Libraries</h2>
                 <p>numPy | pandas | Matplotlib | seaborn</p>
             <h2>Systems</h2>
@@ -48,20 +63,45 @@ const Technical =
     {Classes}
 </div>
 
+const Baseball = 
+<div>
+<h2>Student Manager</h2>
+    <h3>Northeastern University Baseball Team | September 2025 - Present</h3>
+        <p>
+            Currently, I volunteer as a student manager for Northeastern's baseball team. I'm at nearly all practices every week, where
+            I help set up necessary technology and help collect team data for analytics. This means filming during batting practice or manning
+            Yakkertech to keep track of our hitters' exit velocities during batting practice. During scrimmages, I help keep track of every pitch,
+            including the velocity, result (ball/strike), contact type, and whether an out was recorded or a man reached base.
+        </p>
+</div>
+
 const MITRE =
 <div>
-<h2>Full Stack Web Development Co-op</h2>
-    <h3>MITRE Corporation | June - December 2023 </h3>
+<h2>Software Engineering Co-op</h2>
+    <h3>MITRE Corporation | June - December 2023; May 2024 - May 2025</h3>
         <p>
-            I worked on a team of approximately 10 people doing full stack web development in an Agile environment. 
-            During my time as a full stack web development co-op, I gained experience working on both the frontend (Vue)
+            I worked on a team of approximately 10 people in an Agile environment.
+            During my time as a co-op, I gained experience working on both the frontend (Vue)
             and the backend (Node APIs, Python ETL pipelines) of our applications. I worked on updating and maintaining 4
-            company wide web apps. For one of these apps, I wrote and debugged Python and shell scripts to update our ETL pipelines and
-            pull in new data from Denodo and Salesforce to support multiple large releases. For the same app, I also worked on updating the UI
-            to display the data I had pulled in.
+            company wide web apps throughout my time.
+            <br></br>
+            <br></br>
+            During my first co-op at MITRE, for one of these apps, I wrote and debugged Python and shell scripts to update our ETL pipelines and
+            pull in new data from Denodo and Salesforce to support multiple large releases. For the same app, I ensured that the project 
+            was delivered on schedule by also taking on the frontend development work. I also extended an existing API to support project data from Clarity.
+            <br></br>
+            <br></br>
+            During my second co-op at MITRE, I focused mostly on building RAG chatbots in LangChain, as well as helping migrate our apps to
+            the OpenShift cloud platform. I architected and built two MVC chatbots using LangChain, enabling users to chat with data indexes 
+            in Elastic and Azure. I deployed prototypes to OpenShift, allowing users to begin end-to-end testing as soon as possible. I also migrated 
+            a Java API running on Apache Tomcat to OpenShift to save thousands of dollars in on-prem costs, writing Kubernetes manifests for configuration and resource allocation.
+            I also provided other developers on my team with key lessons learned from this migration for future Java app migrations. 
+            <br></br>
+            <br></br>
+            I worked as a full-time co-op from June - December 2023 and May - August 2024, and then worked part time during classes from September 2024 - May 2025.
         </p>
         <p>
-            Relevant skills/technologies: Vue, Node, JavaScript, Python, Bash, Linux, Elasticsearch, Docker, Git
+            Relevant skills/technologies: Vue, Node, JavaScript, Python, Bash, Linux, Elasticsearch, Docker, Git, LangChain, OpenShift, Kubernetes, Apache Tomcat, Cloud Migration
         </p>
 </div>
 
@@ -80,14 +120,19 @@ const Ta =
 const Glr = 
 <div className="glr">
     <h2>Recording Engineer</h2>
-        <h3>Green Line Records | September 2021 - Present</h3>
+        <h3>Green Line Records | September 2021 - May 2025</h3>
             <p> Green Line Records is Northeastern's student-run record label. We sign local artists, record, promote and distribute their music.
                 My responsibilities as a recording engineer include helping set up microphones and ensuring proper signal flow during recording sessions.
                 <br></br>
                 <br></br>
-                Most recently, I served as an assistant engineer to help Main Era record their upcoming song "Ant". Other songs
-                which I have served as an engineer for include <a href="https://open.spotify.com/track/5jC7K0pIhcjY47TOhgCEu7?si=5710aaa783444684" target="_blank" rel="noreferrer">"Chrysanthemums"</a> by Clifford the Band, a project which I served as an
-                engineer-in-training on. I also have some experience recording live sound through helping record the presentations for
+                I have served as an engineer on the following songs:
+                <ul>
+                    <li><a href="https://open.spotify.com/track/16abgjDt7CPNnrsZH1Sysg?si=ea2009aabf114025" target="_blank" rel="noreferrer">Brown Out</a> by Breakfast Beers (2024)</li>
+                    <li><a href="https://open.spotify.com/track/0bpxHsU2yfgSBzBvtbJ9g3?si=470dd9a7077140d4" target="_blank" rel="noreferrer">Ant</a> by Main Era (2023)</li>
+                    <li><a href="https://open.spotify.com/track/5jC7K0pIhcjY47TOhgCEu7?si=5710aaa783444684" target="_blank" rel="noreferrer">Chrysanthemums</a> by Clifford the Band (2023)</li>
+                    <li>Twist the Knife by Girlband, which was scrapped and therefore not released, but I took over lead engineer and mixing duties</li>
+                </ul> 
+                I also have some experience recording live sound through helping record the presentations for
                 MUST 4611 (Music Technology Senior Capstone/Recital) at Northeastern in April 2022.
             </p>
 </div>
@@ -95,6 +140,9 @@ const Glr =
 const Work = 
 <div className="exp-child-work">
     <h1><Fade cascade duration="250" triggerOnce>Work Experience</Fade></h1>
+            <Fade direction="down" triggerOnce>
+            {Baseball}
+            </Fade>
             <Fade direction="down" triggerOnce>
             {MITRE}
             </Fade>
