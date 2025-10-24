09da077acb7cb592b8141938d25b3d715e379476
Deanna Turner <99517267+rawsodium@users.noreply.github.com>
2025-10-24 01:58:49 -0400

    Update projects.js 10-2025

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,7 +10,97 @@ const WebsiteLink =
     <a href="https://github.com/rawsodium/dt-personal-website" target="_blank" rel="noreferrer"><img src={ghLogo} alt="meaningful text" width="50" height="50"></img></a>
   </tr>
 </table>
-  
+
+const CapstoneLinks =
+<table>
+  <tr>
+    <td><a href="https://github.com/rawsodium/tSaturator" target="_blank" rel="noreferrer"><img src={ghLogo} alt="github logo" width="50" height="50"></img></a></td>
+    <td>Read the paper <a href="../../public/DTurnerCapstonePaper.pdf" target="blank" rel="noreferrer">here</a></td>
+  </tr>
+</table>
+
+const Capstone = 
+<div className="small-proj">
+    <h2><Fade cascade duration="150" triggerOnce>tSaturator | February - April 2025 </Fade></h2>
+      <h3>C++, JUCE Framework</h3>
+      <ul>
+        <p>
+          tSaturator (pronounced as saturator) is a tape saturation audio plugin built with C++ and the JUCE framework. 
+          It mimics harmonic distortion and noise from vintage tape machines. tSaturator allows for controlling the amount of saturation, 
+          the drive, and the wet/dry mix of the signal, as well as the amount of noise (or hiss) mixed in with the affected signal. 
+          There is also a spectrum analyzer, allowing users to see what the frequency spectrum looks like post-processing. 
+          It works as both as VST and an AU, meaning it can be used across a variety of digital audio workstations to add some more character to audio.
+          This was my capstone project for my degree.
+        </p>
+      </ul>
+      {CapstoneLinks}
+</div>
+
+const PingPongDelay =
+<div className="small-proj">
+    <h2><Fade cascade duration="150" triggerOnce>Ping-Pong Delay Pedal | March - April 2024 </Fade></h2>
+    <h3>C++, Bela, Breadboard Circuit</h3>
+      <ul>
+        <p>
+          This was the final project for my Embedded Audio Programming (MUST3603) class. We developed a real-time ping-pong delay in C++ using the Bela board.
+          We designed and built circuit with trimmers to control feedback, wet/dry mix, volume and gain, and implemented a low pass filter to control excess 
+          noise from the circuit’s trimmers. This was my first experience working with embedded real-time audio processing. 
+        </p>
+      </ul>
+      <table>
+        <tr><p><a href="https://github.com/rawsodium/ping-pong-delay" target="_blank" rel="noreferrer"><img src={ghLogo} alt="meaningful text" width="50" height="50"></img></a></p></tr>
+      </table>
+</div>
+
+const RoomBooker =
+<div className="small-proj">
+  <h2><Fade cascade duration="150" triggerOnce>Room Booker | March - April 2024 </Fade></h2>
+    <h3>Python, MySQL</h3>
+      <ul>
+        <p>
+          This was the final project for my Database Management Systems (CS5200) class. It uses pymysql to interface with MySQL in order to create
+          a rudimentary mock room booking application for Northeastern classrooms with user authentication. 
+        </p>
+      </ul>
+      <table>
+        <tr><p><a href="https://github.com/rawsodium/CS5200-Final-Project" target="_blank" rel="noreferrer"><img src={ghLogo} alt="meaningful text" width="50" height="50"></img></a></p></tr>
+      </table>
+</div>
+
+const ChatServer =
+<div className="small-proj">
+    <h2><Fade cascade duration="150" triggerOnce>Chat Server | April 2024 </Fade></h2>
+      <h3>C</h3>
+        <ul>
+        <p>
+          A simple chat server using socket programming for my Computer Systems class (CS3650).
+        </p>
+      </ul>
+</div>
+
+const FileSystem =
+<div className="small-proj">
+    <h2><Fade cascade duration="150" triggerOnce>Read-Only File System | March 2024 </Fade></h2>
+      <h3>C</h3>
+        <ul>
+        <p>
+          A simple read-only file system for my Computer Systems class (CS3650). 
+        </p>
+      </ul>
+</div>
+
+const BasicShell =
+<div className="small-proj">
+    <h2><Fade cascade duration="150" triggerOnce>Basic Shell | February 2024 </Fade></h2>
+      <h3>C</h3>
+        <ul>
+        <p>
+          A simple UNIX-style shell for my Computer Systems class (CS3650). It supports commands like cd, pwd, exit, executables, and file input/output redirection.
+        </p>
+      </ul>
+</div>
+
+
 const Website = 
 <div className="small-proj">
     <h2><Fade cascade duration="150" triggerOnce>This website | March 2023 - Present (working on updates)</Fade> </h2>
@@ -37,7 +127,7 @@ const DiscograpyLinks =
 
 const Discograpy = 
 <div className="small-proj">
-    <h2><Fade cascade duration="150" triggerOnce>Discogra.py | March 2023 - Present (in progress)</Fade></h2>
+    <h2><Fade cascade duration="150" triggerOnce>Discogra.py | March 2023</Fade></h2>
       <h3>Python, Jupyter Notebook, numPy, pandas, Matplotlib, seaborn</h3>
         <ul>
           <p>
@@ -82,16 +172,18 @@ const Goose =
 
 const NextProjects = 
 <div className="small-proj">
-  <h2><Fade cascade duration="200" triggerOnce>Projects in the works:</Fade></h2>
-    <h3>Basic Shell | C</h3>
-      <p>A simple UNIX-style shell that I'm working on for my Computer Systems class. It supports commands like cd, pwd, exit, executables, and file input/output redirection.</p>
-    <h3>MBTA Closure Map | Vue, Node, REST APIs, MongoDB</h3>
-      <p>I got the idea for this after the last few large Green Line closures. I wanted a better way to see the entire MBTA subway map 
+    <h3>MusicBox | Swift, Apple Music API</h3>
+      <p>A project I'm working on for my Mobile Application Development (CS5520) class. Think of it as Letterboxd/Goodreads for rating albums.</p>
+    <h3>MBTA Closure Map | Vue, Node, MBTA API, Postgres, vis.js</h3>
+      <p>I got the idea for this after the few large Green Line closures in 2023. I wanted a better way to see the entire MBTA subway map 
         and be able to spot where the closures are, instead of corroborating information through various sources. I'm still working on
-        architecting the data flow, but the general plan is to grab data from MBTA's public API, clean it up, store it in MongoDB and refresh
-        every 12-24 hours or so.</p>
+        architecting the data flow, but the general plan is to grab data from MBTA's public API, clean it up, store it in Postgres as a series of
+        nodes and edges and refreshes every 12-24 hours or so. The map will be done with vis.js, which </p>
+    <h3>Baseball Rivalries | Python (pybaseball)</h3>
+      <p>I wanted to use the pybaseball package to construct a little analysis on the stats behind rivalries in the MLB (I will likely focus on the NL East, 
+         but maybe I'll expand the scope.</p>
     <h3>Win Whamageddon | Python, Spotify API</h3>
-      <p>A small idea I had after losing Whamageddon for a third year in a row. It's a simple python script that removes Wham!'s "Last Christmas" from a user's playlists.</p>
+      <p>A small idea I had after losing Whamageddon for many years in a row. It's a simple python script that removes Wham!'s "Last Christmas" from a user's playlists.</p>
 </div>
 
 const Pad = 
@@ -101,13 +193,16 @@ const Pad =
 function Projects() {
     return (
         <div className="project-parent">
-          {Website}
           {NextProjects}
-          {Pad}
+          {Capstone}
+          {PingPongDelay}
+          {RoomBooker}
+          {ChatServer}
+          {FileSystem}
+          {BasicShell}
+          {Website}
           {Discograpy}
           {ImageProcessor}
-          {Pad}
-          {Pad}
           {Goose}
         </div>
       );
